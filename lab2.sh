8230191cc6e23e1a5779d0aafea13bd89f94486c
EdgarAbramian <91754043+EdgarAbramian@users.noreply.github.com>
2025-02-28 07:38:06 +0000

    ашч

---
+++ lab2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=omp_mpi
+#SBATCH --time=03:00:00
+#SBATCH --nodes=2 --ntasks-per-node=8
+#SBATCH --mem=8gb
+
+module load intel/icc18
+module load intel/mpi4
+
+
+gcc -fopenmp -o omp_2 lab2_omp.cpp
+mpicc - 
+
+
+export I_MPI_LIBRARY=/usr/lib64/slurm/mpi_pmi2.so
+./omp_2 4 1
+./omp_2 8 1
+./omp_2 16 1
+
+
+srun --mpi=pmi2 -n 4 ./mpi_2 1
+srun --mpi=pmi2 -n 8 ./mpi_2 1
